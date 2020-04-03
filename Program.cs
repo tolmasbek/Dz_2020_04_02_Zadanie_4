a39add63780495ab1673b3c7e997484639a165f7
tolmasbek <62470053+tolmasbek@users.noreply.github.com>
2020-04-03 14:58:32 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace dz_04_02_zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  • Задание 4
+             *      A. Создать проект по шаблону Console Application.
+             *      B. Требуется: создать класс которое описывает состояние и поведение некой сущности.
+             *      C. Сущность возьмите из обыденности.
+             */
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
